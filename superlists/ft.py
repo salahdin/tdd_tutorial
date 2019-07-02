f9c13b2c67247631e4042fa4a47c905a192d7fd2
salahdin <salahdinjemal@gmail.com>
2019-07-02 11:57:54 +0200

    Update ft.py

--- superlists/ft.py
+++ superlists/ft.py
@@ -14,11 +14,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+        #user browses to out page
         self.browser.get(' http://127.0.0.1:8000/')
 
-        # She notices the page title and header mention to-do lists
+        # header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         # self.fail('finish the test')  # fails no matter what , we are only using to display a message on here
 
