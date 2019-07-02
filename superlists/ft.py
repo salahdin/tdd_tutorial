cdf326d850f746b89a62c7f801206752524f6eca
salahdin zeberga <salahdinzeberga@admins-MacBook-Air-7.local>
2019-07-02 11:54:35 +0200

    add new tests

--- superlists/ft.py
+++ superlists/ft.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,11 +16,24 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(' http://127.0.0.1:8000/')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('finish the test')  # fails no matter what , we are only using to display a message on here
+        # self.fail('finish the test')  # fails no matter what , we are only using to display a message on here
+
+        # using method selenium provides that finds elements of an html by the tag name to capture the header text
+        header_text =  self.browser.find_element_by_tag_name('h1').text
+        # assertIn returns true if To-do is in the headers
+        self.assertIn('To-do', header_text)
+
+        # user finds a tab to add new to-do
+        # capture an html element by the id
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        # if the place holder of the html input field is the same as the second argument return true
+        self.assertAlmostEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        self.fail('end of test')
 
 
 if __name__ == '__main__':
--- superlists/list/views.py
+++ superlists/list/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http.response import HttpResponse
 # Create your views here.
+
+
+def home_page(request):
+    html = "<html><title>To-Do</title></html>"
+
+    return render(request ,'index.html',{'hi': 3})
